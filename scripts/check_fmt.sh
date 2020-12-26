d8b791eabee18a482507b1b2456ae198cf24fd9d
Christopher Szatmary <cs@christopherszatmary.com>
2020-12-25 22:12:11 -0500

    Add makefile and setup CI (#3)

---
+++ scripts/check_fmt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$(shed run goimports -- -l . | wc -l)" -gt 0 ]; then
+    echo "Unformatted files found:"
+    # Run again to print files. Couldn't figure out a way to
+    # save the output and make it work with wc.
+    shed run goimports -- -l .
+    exit 1
+fi
